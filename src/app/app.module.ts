4cfe6e2783630e4f844ac197acf7cf9bf44c34d2
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2018-11-26 18:20:44 +0530

    design improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
