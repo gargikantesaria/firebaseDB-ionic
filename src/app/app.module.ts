ba6edb0208b0778eac28ab5ac5c8b7d931f1b175
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2018-11-26 17:36:56 +0530

    basic CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,18 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC5Y8jtHsEluTXUUwt-M-sPN7gEWH-j0iY",
+  authDomain: "usedb-cbfc7.firebaseapp.com",
+  databaseURL: "https://usedb-cbfc7.firebaseio.com",
+  projectId: 'usedb-cbfc7',
+  messagingSenderId: '180101044536'
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +23,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,63 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  itemRef: AngularFireObject<any>;
+  item: Observable<any>;
+  name; displayDetail; Updatedname;ref;
 
+  constructor(public navCtrl: NavController,  public afDB: AngularFireDatabase) {
+    this.itemRef = afDB.object('item');
+    this.ref = this.afDB.database.ref('/item');
+    this.ref.on('child_added', (res) => { console.log(" from child_add", res) })
+    this.ref.on('child_changed', (res) => { console.log("child changed", res) });
+    this.ref.on('child_moved', (res) => console.log("child_moved",res));
+    this.ref.on('child_removed', (res) => { console.log("Child_removed", res)});
+    // this.item = this.itemRef.valueChanges();
+    this.getDetails();  
   }
 
+  updateData(add, updatedName){
+    this.afDB.list('/item').snapshotChanges().subscribe((res) => {
+      res.forEach((ele:any) => {
+        if(ele.payload.val().name == add) {
+           this.afDB.list('/item').update(ele.key,{ name: updatedName }).then(() => {
+            this.Updatedname = "";
+            this.name = "";
+            add = "";
+            updatedName = "";
+           })
+        }
+      });
+    });
+  }
+
+  addData(addName) {
+    this.ref.push({ name: addName }).then(() =>{ this.name = ""; addName =""; })  
+  }
+
+  deleteData(removeName){
+
+    this.afDB.list('/item').snapshotChanges().subscribe((res) => {
+      res.forEach((ele:any) => {
+        if(ele.payload.val().name == removeName) {
+           this.afDB.list('/item').remove(ele.key).then(() => {
+            this.Updatedname = "";
+            this.name = "";
+            removeName = "";
+           })
+        }
+      });
+    });
+  }
+
+  getDetails(){
+    this.afDB.list('/item').snapshotChanges().subscribe((res) => {
+      let a:any = [];
+      res.forEach((ele) => {
+        a.push(ele.payload.val())
+      });
+      this.displayDetail = a;
+    })
+  }
+ 
 }
